8b7d58dc0a9e9dc08a939ed57d40baad7b56bc0d
Jason Nordheim <jason.nordheim@gmail.com>
2021-03-03 21:27:35 -0700

    added config from refactor

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,11 @@
-
-
 // packages
 import { MikroORM } from "@mikro-orm/core";
 
-
+// config
+import config from "./mikro-orm.config";
 
 const Main = async () => {
-  const Orm = await MikroORM.init();
+  const Orm = await MikroORM.init(config);
 };
 Main().catch((reason) => {
   console.error(reason);
